b233a7a7340ead7780a00d804cf43f8285b32c04
ThienBKDN <takhanhthient3@GMAIL.COM>
2023-04-15 21:21:42 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: 'https://moluy.com',
+      },
+      {
+        source: '/:slug*',
+        destination: 'https://moluy.com/:slug*',
+      },
+      {
+        source: '/doc-bao/:slug*',
+        destination: 'https://moluy.com/doc-bao/:slug*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
