513461c0ce3f28cb6ba9e48dc0724b770624fd3e
ThienBKDN <takhanhthient3@GMAIL.COM>
2023-04-15 09:23:35 +0700

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    window.location.href = pageProps?.url;
-  }, []);
-
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
